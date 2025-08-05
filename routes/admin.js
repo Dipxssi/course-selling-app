2c961003d4b65283210c564cefd27e59252c0a14
DIPSHA <dipshadas4@gmail.com>
2025-08-05 20:50:36 +0530

    Added auth endpoints for users and admins

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,74 @@
 const {Router} = require("express");
-
+const {z} = require("zod")
+const bcrypt = require("bcrypt")
 const adminRouter = Router();
-
+const jwt = require("jsonwebtoken")
 const {adminModel} = require("../db")
 
-adminRouter.post('/signup',function(req,res){
+adminRouter.post('/signup', async function(req,res){
   
+  const requiredBody = z.object({
+     email : z.email(),
+     password : z.max(10).min(5).String(),
+     firstName : z.max(10).min(5).String(),
+     lastName : z.max(10).min(5).String(),
+  })
+
+   const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+   if(!parsedDataWithSuccess.success){
+    res.json({
+      mssg: "Incorrect format",
+      error: parsedDataWithSuccess.error
+    })
+    return
+   }
+
+   const email = req.body.email;
+   const password = req.body.password;
+   const firstName = req.body.firstName;
+   const lastName = req.body.lastName;
+  
+   const hashedPassword = await bcrypt.hash(password,2);
+
+   await adminModel.create({
+    email : email,
+    password : hashedPassword,
+    firstName : firstName,
+    lastName : lastName
+   })
+  res.json({
+    mssg: "Signed up"
+  })
 });
 
-adminRouter.post('/signin',function(req,res){
+adminRouter.post('/signin',async function(req,res){
+   const {email , password} = req.body;
 
+   const response = await adminModel.findOne({
+      email : email
+   })
+
+   if(!response){
+    res.status(403).json({
+      mssg: "User dosen't exists in db"
+    })
+    return 
+   }
+   const passwordMatch = await bcrypt.compare(password,response.password)
+
+   if(passwordMatch){
+     const token = jwt.sign({
+          id : response._id
+     },process.env.JWT_ADMIN_SECRET)
+     res.json({
+      token : token
+     })
+   } else{
+    res.status(403).json({
+      mssg : "Incorrect credentials"
+    })
+   }
 });
 
 adminRouter.post('/course',function(req,res){
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,71 @@
 const {Router} = require("express");
-
+const {z} = require("zod")
+const bcrypt = require("bcrypt")
 const userRouter = Router();
+const jwt = require("jsonwebtoken")
 
 const {userModel} = require("../db")
 
-userRouter.post('/signup',function(req,res){
-  
+userRouter.post('/signup',async function(req,res){
+   const requiredBody = z.object({
+    email : z.email(),
+    password :z.max(100).min(5).string() ,
+    lastName :z.max(100).min(5).string() ,
+    firstName : z.max(100).min(5).string() 
+   })
+
+   const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+   if(!parsedDataWithSuccess.success){
+    res.json({
+      mssg : "Incorrect format",
+      error:parsedDataWithSuccess.error
+    })
+    return 
+   }
+
+  const {email,password, firstName , lastName} = req.body;
+  const hashedPassword = await bcrypt.hash(password,2);
+
+  await userModel.create({
+    email : email,
+    password : hashedPassword,
+    lastName : lastName,
+    firstName : firstName
+  })
+  res.json({
+    mssg : "You are signed up"
+  })  
 });
 
-userRouter.post('/signin',function(req,res){
+userRouter.post('/signin',async function(req,res){
+   
+  const {email , password} = req.body;
+
+  const response =  await userModel.findOne({
+    email: email
+  })
+
+  if(!response){
+    return res.status(401).json({
+      mssg: "User not found"
+    })
+  }
+  const passwordMatch = await bcrypt.compare(password,response.password);
+
+  if(passwordMatch){
+    const token = jwt.sign({
+      id : response._id
+    },process.env.JWT_SECRET)
+    res.json({
+      token : token
+    })
+  } else {
+    res.status.json({
+      mssg : "Wrong creds"
+    })
+  }
+
 
 });
 
