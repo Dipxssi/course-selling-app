7cc110612eb4500d1cf0a0cc8a7a15b4d8b7e032
DIPSHA <dipshadas4@gmail.com>
2025-08-05 12:32:39 +0530

    Router in express

--- index.js
+++ index.js
@@ -2,31 +2,14 @@ const express = require("express");
 const jwt = require("jsonwebtoken")
 const JWT_SECRET = "randomdas"
 
+const {courseRouter} = require('./routes/course')
+const {userRouter} = require('./routes/user')
+
 const app = express();
-
-
 
 app.use(express.json())
 
-app.post('/user/signup',function(req,res){
-  
-});
-
-app.post('/user/signin',function(req,res){
-
-});
-
-app.get('/user/purchases',function(req,res){
-
-});
-
-app.post('/course/purchase',function(req,res){
-
-});
-
-app.get('/courses',function(req,res){
-
-});
-
+app.use("/user",userRouter)
+app.use("/course",courseRouter)
 
 app.listen(3000);
