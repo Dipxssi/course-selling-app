95a9dff9dcb05b223d54ba0536dce282e2471446
DIPSHA <dipshadas4@gmail.com>
2025-08-05 12:08:42 +0530

    Added the skeleton route

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "randomdas"
+
+const app = express();
+
+
+
+app.use(express.json())
+
+app.post('/user/signup',function(req,res){
+  
+});
+
+app.post('/user/signin',function(req,res){
+
+});
+
+app.get('/user/purchases',function(req,res){
+
+});
+
+app.post('/course/purchase',function(req,res){
+
+});
+
+app.get('/courses',function(req,res){
+
+});
+
+
+app.listen(3000);
